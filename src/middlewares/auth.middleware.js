15872e8ebfb7634fd4a8521df9ba9a790a642d0c
Pankajol <pankaj.yadav16228@sakec.ac.in>
2024-02-01 08:14:50 +0530

    login and logout throw the accesstoken and refresh Tokens

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asynhandler";
+import jwt from "jsonwebtoken"
+import {User} from "../models/user.model"
+
+
+export const verifyJWT = asyncHandler( async (req,_,next) => {
+ try {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
+   
+    if(!token){
+       throw new ApiError(401,"Unauthorizes request")
+    }
+   
+   const decodeedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+   const user = await User.findById(decodeedToken?._id).select("-password -refreshToken")
+   
+   if(!user){
+       // TOdo discuss about
+       throw new ApiError(401,"invalid access token")
+   }
+   
+   req.user = user;
+   next()
+ } 
+ catch (error) {
+    throw new ApiError(401,error?.message || "invalid access token")
+ }
+
+})
